37b90915a734fb3ea739de1cf7c2f606e92efde3
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:50:13 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhap vao mot chuoi:");
+        string inputString = Console.ReadLine();
+
+        Console.WriteLine("Nhap vao ky tu can dem:");
+        char targetChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        int count = CountOccurrences(inputString, targetChar);
+
+        Console.WriteLine($"So lan xuat hien cua ky tu '{targetChar}' trong chuoi la: {count}");
+    }
+
+    static int CountOccurrences(string inputString, char targetChar)
+    {
+        int count = 0;
+        foreach (char c in inputString)
+        {
+            if (c == targetChar)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
